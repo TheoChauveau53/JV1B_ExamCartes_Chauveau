1e906101e271e7f3d36d92b7f048b44622e1a4c7
Fievre <118454473+TheoChauveau53@users.noreply.github.com>
2023-12-13 15:18:02 +0100

    final

--- jeu.py
+++ jeu.py
@@ -7,6 +7,8 @@ class Carte:
         return self.__manacost
     def getName(self):
         return self.__name
+    def getDescription(self):
+        return self.__description
 
 class Mage:
     def __init__(self,name,PV,total,mana,main,zone,defausse):
@@ -37,6 +39,10 @@ class Mage:
         for i in range(len(self.__zone)):
             temp.append(self.__zone[i].getName())
         return temp
+    def getName(self):
+        return self.__name
+    def GagneMana(self,nombre):
+        self.__mana += nombre
 
 class Cristal(Carte):
     def __init__(self,mana,name,description,valeur):
@@ -44,6 +50,8 @@ class Cristal(Carte):
         self.__valeur = valeur
     def jouer(self,mage):
         mage.zone(self)
+    def gagneMana(self,mage):
+        mage.GagneMana(self.__valeur)
 
 class Creature(Carte):
     def __init__(self,mana,name,description,pv,attaque):
@@ -52,14 +60,21 @@ class Creature(Carte):
         self.__attaque = attaque
     def jouer(self,mage):
         mage.zone(self)
+    def attaquer(self,cible):
+        cible.perdPv(self.__attaque)
+    def perdPv(self,nombre,mage):
+        self.__pv -= nombre
+        if self.__pv<=0:
+            mage.defausse(self)
 
 
 class Blast(Carte):
     def __init__(self,mana,name,description,valeur):
         Carte.__init__(self,mana,name,description)
         self.__valeur = valeur
-    def jouer(self,mage):
+    def jouer(self,mage,cible):
         mage.defausse(self)
+        cible.perdPv(self.__valeur)
 
 cristal1 = Cristal(4,"cristal1","un cristal qui donne 1 mana",1)
 mage1 = Mage("mage1",100,100,5,[cristal1],[],[])
