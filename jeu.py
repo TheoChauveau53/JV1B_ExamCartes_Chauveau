55df1964d81f3fd3c8a984950482b4af522ae789
Fievre <118454473+TheoChauveau53@users.noreply.github.com>
2023-12-13 14:53:48 +0100

    Create jeu.py

---
+++ jeu.py
@@ -0,0 +1,72 @@
+class Carte:
+    def __init__(self,mana,name,description):
+        self.__manacost = mana
+        self.__name = name
+        self.__description = description
+    def getManacost(self):
+        return self.__manacost
+    def getName(self):
+        return self.__name
+
+class Mage:
+    def __init__(self,name,PV,total,mana,main,zone,defausse):
+        self.__name = name
+        self.__PV = PV
+        self.__total = total
+        self.__mana = mana
+        self.__main = main
+        self.__zone = zone
+        self.__defausse = defausse
+    def jouer(self,carte):                  #quand on joue une carte
+        self.__mana-= self.__main[carte].getManacost()
+        self.__main[carte].jouer(self)
+        self.__main.pop(carte)
+    def defausse(self,carte):               #appelé quand on joue une carte blast
+        self.__defausse.append(carte)
+    def zone(self,carte):                   #appelé quand on joue une carte cristal ou créature
+        self.__zone.append(carte)
+    def PerdPv(self,PVperdus):
+        self.__PV -= PVperdus
+    def getmain(self):
+        temp = []
+        for i in range(len(self.__main)):
+            temp.append(self.__main[i].getName())
+        return temp
+    def getzone(self):
+        temp = []
+        for i in range(len(self.__zone)):
+            temp.append(self.__zone[i].getName())
+        return temp
+
+class Cristal(Carte):
+    def __init__(self,mana,name,description,valeur):
+        Carte.__init__(self,mana,name,description)
+        self.__valeur = valeur
+    def jouer(self,mage):
+        mage.zone(self)
+
+class Creature(Carte):
+    def __init__(self,mana,name,description,pv,attaque):
+        Carte.__init__(self,mana,name,description)
+        self.__pv = pv
+        self.__attaque = attaque
+    def jouer(self,mage):
+        mage.zone(self)
+
+
+class Blast(Carte):
+    def __init__(self,mana,name,description,valeur):
+        Carte.__init__(self,mana,name,description)
+        self.__valeur = valeur
+    def jouer(self,mage):
+        mage.defausse(self)
+
+cristal1 = Cristal(4,"cristal1","un cristal qui donne 1 mana",1)
+mage1 = Mage("mage1",100,100,5,[cristal1],[],[])
+
+
+print(mage1.getmain())
+print(mage1.getzone())
+mage1.jouer(0)
+print(mage1.getmain())
+print(mage1.getzone())
